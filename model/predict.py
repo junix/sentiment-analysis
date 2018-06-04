641b0c5011fead8451bef73bf471ae7ad36d2fe8
wanglijun <juns1984@qq.com>
2018-06-04 08:07:00 +0800

    init

--- model/predict.py
+++ model/predict.py
@@ -1,21 +1,27 @@
+import os
 import torch
 import torch.nn.functional as F
 
 from conf import run_device
-from .train import dump_path
 from .net import Net
+
+dump_path = os.path.dirname(os.path.abspath(__file__)) + '/model.pt'
+
+
+def predict(net, text):
+    net.eval()
+    with torch.no_grad():
+        hidden = net.init_hidden()
+        score = net.forward(text, hidden)
+        score = F.sigmoid(score).item()
+        return 1 if score > 0.5 else 0
 
 
 def load_predict():
     net = Net.load(dump_path)
-    net.eval()
     net.move_to_device(run_device())
 
-    def predict(text):
-        with torch.no_grad():
-            hidden = net.init_hidden()
-            score = net.forward(text, hidden)
-            score = F.sigmoid(score).item()
-            return 1 if score > 0.5 else 0
+    def _pred(text):
+        return predict(net, text)
 
-    return predict
+    return _pred
--- model/train.py
+++ model/train.py
@@ -9,6 +9,7 @@ from yxt_nlp_toolkit.embedding.general import WordEmbedding
 from dataset.imdb import *
 from conf import run_device
 from .net import Net
+from .predict import predict
 
 dump_path = os.path.dirname(os.path.abspath(__file__)) + '/model.pt'
 _glove_path = os.path.expanduser('~/nlp/glove.6B.200d.txt')
@@ -22,7 +23,6 @@ def train_and_dump(min_count=5):
     lang.build_embedding(wv=embedding, out_embedding=weight)
     net.embedding.weight.data = torch.tensor(weight, dtype=torch.float)
 
-    net.train()
     net.move_to_device(run_device())
     do_train(net)
 
@@ -36,6 +36,7 @@ def do_train(net):
     total_loss = .0
     for epoch in range(5):
         for text, label in train_dataset:
+            net.train()
             text = text[0]
             net.zero_grad()
             hidden = net.init_hidden()
@@ -47,8 +48,20 @@ def do_train(net):
             total_loss += loss.detach_().item()
             total_count += 1
             if total_count % 1000 == 0:
-                print(total_loss)
+                print(total_loss, 'accu=', accu(net, test_dataset))
                 total_loss = .0
             if total_count % 5000 == 0:
                 net.dump(dump_path)
     net.dump(dump_path)
+
+
+def accu(net, test_dataset):
+    total = float(len(test_dataset))
+    right_cnt = 0
+    for text, label in test_dataset:
+        net.eval()
+        result = predict(net, text[0])
+        label = label.long().item()
+        if result == label:
+            right_cnt += 1
+    return right_cnt / total
